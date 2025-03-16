191996196001961648a0870978328cd4c13cdd91
Juan Manuel <juanmanuelperlazaperez@gmail.com>
2025-03-16 13:29:41 -0500

    add API service for user login and registration

---
+++ src/services/api.js
@@ -0,0 +1,42 @@
+const BACKEND_URL = "http://localhost:4000";
+
+export async function loginUser(user) {
+  try {
+    const data = await fetch(`${BACKEND_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!data.ok) {
+      return false;
+    }
+
+    const response = await data.json();
+    localStorage.setItem("user", response);
+    return response;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function registerUser(user) {
+  try {
+    const data = await fetch(`${BACKEND_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!data.ok) {
+      return false;
+    }
+    return await data.json();
+  } catch (error) {
+    return null;
+  }
+}
