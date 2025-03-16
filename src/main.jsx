eac4c68ab62f4b2545fff8d41dc704e87b18d0ac
Juan Manuel <juanmanuelperlazaperez@gmail.com>
2025-03-16 14:41:06 -0500

    add Router integration in main entry point for improved routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import RouterDom from "./routes/RouterDom.jsx"; // Updated extension
 
